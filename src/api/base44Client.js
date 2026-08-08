caf9000f25f2291823edf2e7263037df4e1ce8f6
Eimy Herrer <fck.police.228@gmail.com>
2026-08-08 13:11:01 +0200

    refactor: remove legacy Base44 client

--- src/api/base44Client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@base44/sdk";
-import { appParams } from "@/lib/app-params";
-
-const appId = appParams.appId || import.meta.env.VITE_BASE44_APP_ID;
-export const base44 = createClient({ appId });
-if (appParams.token) base44.auth.setToken(appParams.token);
