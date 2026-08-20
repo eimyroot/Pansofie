0d61f23fe696aea392024ad0b44a3847d3e317da
Eimy Herrer <fck.police.228@gmail.com>
2026-08-20 18:04:02 +0200

    R6: load luminous network color language

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App.jsx";
 import "@/index.css";
+import "@/luminous-network-r6.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
