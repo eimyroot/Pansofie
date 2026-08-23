d0584431b994153ab5e5a6357e6bc59497214524
Eimy Herrer <fck.police.228@gmail.com>
2026-08-24 00:02:02 +0200

    R14 taste: expose stable anchor for public navigation

--- src/components/pansofie/ParticipationCTA.jsx
+++ src/components/pansofie/ParticipationCTA.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import PansofieTaste from "@/components/pansofie/PansofieTaste";
 
 export default function ParticipationCTA() {
-  return <PansofieTaste />;
+  return <div id="ochutnejte" className="scroll-mt-28"><PansofieTaste /></div>;
 }
