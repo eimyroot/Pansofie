25d09386866a60d9b3f4f9ddd1dd9c06919303ec
Eimy Herrer <fck.police.228@gmail.com>
2026-08-23 23:53:52 +0200

    R14 show-don't-sell: load open bridge and CTA override styles

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import "@/audience-detail-r14.css";
 import "@/participation-network-r14.css";
 import "@/participation-cta-r14.css";
 import "@/pansofie-taste-r14.css";
+import "@/material-bridge-open-r14.css";
+import "@/show-dont-sell-r14.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
