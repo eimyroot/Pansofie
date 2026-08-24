74e991676ee50db0ffd060e8f5be62c22da50c7b
Eimy Herrer <fck.police.228@gmail.com>
2026-08-24 05:25:45 +0200

    R25 place company dashboard above governed partner workflow

--- src/pages/PartnerWorkspace.jsx
+++ src/pages/PartnerWorkspace.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import PartnerAllianceDashboard from "@/components/pansofie/PartnerAllianceDashboard";
 import PartnerHub from "@/pages/PartnerHub";
 import PartnerReviewPanel from "@/components/pansofie/PartnerReviewPanel";
 
 export default function PartnerWorkspace() {
   return (
     <>
-      <PartnerHub />
-      <div className="product-shell pt-0">
+      <PartnerAllianceDashboard />
+      <div id="partner-challenge-workflow" className="scroll-mt-8"><PartnerHub /></div>
+      <div id="partner-r5-review" className="product-shell pt-0 scroll-mt-8">
         <PartnerReviewPanel />
       </div>
     </>
