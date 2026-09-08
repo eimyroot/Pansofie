8e008f7d67a83218777e9b534d55e5979ae0e68c
eimyroot <eimyherrer@gmail.com>
2026-09-08 13:22:15 +0200

    R15: load tracked homepage asset bindings

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "./r9-stability.css";
 import "./artkit-v1.css";
 import "./product.css";
 import "./home-final.css";
+import "./home-final-assets.css";
 import App from "./App";
 import { PansofieProvider } from "./state/PansofieContext";
 import { LanguageProvider } from "./state/LanguageContext";
